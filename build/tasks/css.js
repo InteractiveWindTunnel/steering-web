b2384d3d7e91a29ccd660a8eb771f2d504397331
Dawid Królak <taavit@gmail.com>
2015-07-16 19:35:57 +0200

    fixed css task

--- build/tasks/css.js
+++ build/tasks/css.js
@@ -1,14 +1,16 @@
 var gulp = require('gulp');
-var sass = require('gulp-ruby-sass') ;
+var sass = require('gulp-sass') ;
 var paths = require('../paths');
 
 gulp.task('css', function() { 
-    return sass(paths.sassPath + '/style.scss', {
-             style: 'compressed',
-             loadPath: [
-                 './app/assets/scss',
-                 paths.jspmPath + '/github/twbs/bootstrap-sass@3.3.5/assets/stylesheets'
-             ]
-         })
-        .pipe(gulp.dest('./web/assets/css')); 
+    return gulp
+        .src(paths.sassPath + '/**/*.scss')
+        .pipe(
+            sass({
+                includePaths: [
+                    paths.jspmPath + '/github/twbs/bootstrap-sass@3.3.5/assets/stylesheets',
+                    './app/assets/scss'
+                ]
+            }))
+            .pipe(gulp.dest('./web/assets/css')); 
 });
